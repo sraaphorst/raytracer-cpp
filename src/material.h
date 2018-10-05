f8d56ea2b589e2574e88db85f4b18123cb764b66
sraaphorst <sraaphorst@gemini.edu>
2018-10-05 13:04:04 -0300

    Cleaning up and adding files.

--- src/material.h
+++ src/material.h
@@ -6,21 +6,34 @@
 
 #pragma once
 
+#include "vector.h"
+
 namespace raytracer {
     class Material final {
     private:
+        const Colour colour;
         const double ambient;
         const double diffuse;
         const double specular;
         const double shininess;
 
     public:
-        constexpr Material(double ambient, double diffuse, double specular, double shininess) noexcept:
-            ambient{ambient}, diffuse{diffuse}, specular{specular}, shininess{shininess} {}
-        constexpr Material() noexcept: Material{0, 0, 0, 0} {}
+        constexpr Material(const Colour colour,
+                           double ambient,
+                           double diffuse,
+                           double specular,
+                           double shininess) noexcept:
+            colour{colour}, ambient{ambient}, diffuse{diffuse}, specular{specular}, shininess{shininess} {}
+
+        constexpr Material() noexcept:
+            Material{DEFAULT_COLOUR, DEFAULT_AMBIENT, DEFAULT_DIFFUSE, DEFAULT_SPECULAR, DEFAULT_SHININESS} {}
+
         constexpr Material(const Material&) = default;
         constexpr Material(Material&&) = default;
 
+        constexpr Colour getColour() const noexcept {
+            return colour;
+        }
         constexpr double getAmbient() const noexcept {
             return ambient;
         }
@@ -33,5 +46,11 @@ namespace raytracer {
         constexpr double getShininess() const noexcept {
             return shininess;
         }
+
+        constexpr static Colour DEFAULT_COLOUR    = predefined_colours::white;
+        constexpr static double DEFAULT_AMBIENT   = 0.1;
+        constexpr static double DEFAULT_DIFFUSE   = 0.9;
+        constexpr static double DEFAULT_SPECULAR  = 0.9;
+        constexpr static double DEFAULT_SHININESS = 200;
     };
 }
--- test/TestMaterial.cpp
+++ test/TestMaterial.cpp
@@ -4,3 +4,17 @@
  * By Sebastian Raaphorst, 2018.
  */
 
+#include <catch.hpp>
+
+#include "material.h"
+
+using namespace raytracer;
+
+TEST_CASE("The default material") {
+    constexpr Material m;
+    REQUIRE(m.getColour() == make_colour(1, 1, 1));
+    REQUIRE(ALMOST_EQUALS(m.getAmbient(), 0.1));
+    REQUIRE(ALMOST_EQUALS(m.getDiffuse(), 0.9));
+    REQUIRE(ALMOST_EQUALS(m.getSpecular(), 0.9));
+    REQUIRE(m.getShininess() == 200);
+}
