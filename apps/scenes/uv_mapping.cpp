59d30f47e5b40dfc928853798b85baf4e2cb8d0e
sraaphorst <sraaphorst@gemini.edu>
2018-12-07 12:42:35 -0300

    Added notes about UV mapping.

---
+++ apps/scenes/uv_mapping.cpp
@@ -0,0 +1,51 @@
+/**
+ * uv_mapping.cpp
+ *
+ * By Sebastian Raaphorst, 2018.
+ */
+
+#include <fstream>
+#include <vector>
+
+#include <camera.h>
+#include <canvas.h>
+#include <constmath.h>
+#include <material.h>
+#include <pointlight.h>
+#include <sphere.h>
+#include <patterns/checkerpattern.h>
+#include <vec.h>
+#include <world.h>
+
+#include "../timer.h"
+
+using namespace raytracer;
+using namespace raytracer::shapes;
+
+int main() {
+//    Camera camera{800, 400, math_constants::pi_by_six<>,
+//            view_transform(make_point(40, 0, -70), make_point(0, 0, -5), predefined_tuples::y1)};
+    Camera camera{400, 400, math_constants::pi<> / 10,
+            view_transform(make_point(0, 0, 0), make_point(0, 0, 1), predefined_tuples::y1)};
+
+    PointLight light{make_point(0, 10, -10), predefined_colours::white};
+
+    // Ball 1 material
+    auto ball1_pattern = std::make_shared<CheckerPattern>(make_colour(0.3, 1.0, 0.3),
+                                                          make_colour(1.0, 1.0, 0.3));
+    ball1_pattern->setTransformation(rotation_y(math_constants::pi_by_three<>) * scale(0.25, 0.25, 0.25));
+    auto ball1_materail = std::make_shared<Material>(ball1_pattern);
+    auto ball1 = Sphere::createSphere();
+    ball1->setMaterial(ball1_materail);
+    ball1->setTransformation(scale(5, 5, 5) * translation(0, 0, 10));
+
+    std::vector<std::shared_ptr<Shape>> shapes = { ball1 };
+    World world{light, shapes};
+
+    run_timed("uv_mapping", [&camera, &world] {
+        auto canvas = camera.render(world);
+        std::ofstream out("uv_mapping.ppm");
+        out << canvas;
+        out.close();
+    });
+}
--- src/patterns/checkerpattern.cpp
+++ src/patterns/checkerpattern.cpp
@@ -14,6 +14,15 @@ namespace raytracer {
             colour1{predefined_colours::white},
             colour2{predefined_colours::black} {}
 
+    // TODO: In order to do this with UV mapping for a sphere, we'll need to get the normal
+    //  at point, and then switch directions to get a vector to the origin of the sphere.
+    //  We will probably need some calculation here to get the vector v to the right length.
+    //  Then we use the formulae on Wikipedia at:
+    //  https://en.wikipedia.org/wiki/UV_mapping
+    //  u = 0.5 + arctan2(d_z, d_x) / 2pi
+    //  v = 0.5 = arcsin(d_y) / pi
+    //  Then we use the (u, v), coordinates to determine the colour in the equivalent
+    //  2D manner as the 3D calculation below.
     const Colour CheckerPattern::colourAt(const Tuple &point) const noexcept {
         const auto idx =
                 (static_cast<size_t>(floor(point[tuple_constants::x])) +
